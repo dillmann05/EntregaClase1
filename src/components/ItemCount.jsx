2f641b4ec9af96c91ba94bb336c64e9360098785
dillmann05 <enzo.dillmanntv@gmail.com>
2022-09-17 16:11:30 -0300

    countcorreciones

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -3,17 +3,17 @@ import { AiOutlineLine } from "react-icons/ai"
 import { IoAddOutline } from "react-icons/io5"
 
 const ItemCount = ({stock, initial}) => {
-  const [item, setItem] = useState(initial);
+  const [count, setCount] = useState(initial);
 
   const onSubtract = () => {
-    if (item > 1) {
-      setItem( item - 1 );
+    if (count > initial) {
+      setCount( count - 1 );
     }
   };
 
   const onAdd = () => {
-    if (item < stock) {
-      setItem( item + 1 );
+    if (count < stock) {
+      setCount( count + 1 );
     }
   };
 
@@ -23,7 +23,7 @@ const ItemCount = ({stock, initial}) => {
           <button className="btn btn-circle btn-outline" onClick={onSubtract}>
               <AiOutlineLine/>
           </button>
-          <strong className="text-xl "> {item} </strong>
+          <strong className="text-xl "> {count} </strong>
           <button className="btn btn-circle btn-outline" onClick={onAdd}>
               <IoAddOutline className="text-2xl"/>
           </button>
